d76f824ae4e2ff71fd10d30e5ed17054a5f79827
Zoran Horvat <zh@codinghelmet.com>
2023-04-16 20:12:25 +0200

    Improved data seeding to serialize creation of multiple entities

--- Source/Bookstore/Data/Seeding/IDataSeed.cs
+++ Source/Bookstore/Data/Seeding/IDataSeed.cs
@@ -5,6 +5,11 @@ public interface IDataSeed<TEntity>
     Task SeedAsync();
     Task<TEntity> EnsureEqualExists(TEntity entity);
 
-    async Task<IEnumerable<TEntity>> EnsureEqualExists(IEnumerable<TEntity> entities) =>
-        await Task.WhenAll(entities.Select(EnsureEqualExists));
+    async Task<IEnumerable<TEntity>> EnsureEqualExists(IEnumerable<TEntity> entities)
+    {
+        List<TEntity> result = new();
+        foreach (TEntity entity in entities)
+            result.Add(await EnsureEqualExists(entity));
+        return result;
+    }
 }
