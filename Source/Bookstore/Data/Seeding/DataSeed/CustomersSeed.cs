9eb00289f2ae1849b64d5a58cc0bd26f7abc96bb
Zoran Horvat <zh@codinghelmet.com>
2023-04-28 12:21:02 +0200

    Added seed data for customers

---
+++ Source/Bookstore/Data/Seeding/DataSeed/CustomersSeed.cs
@@ -0,0 +1,39 @@
+using Bookstore.Domain.Models;
+
+namespace Bookstore.Data.Seeding.DataSeed;
+
+public class CustomersSeed : IDataSeed<Customer>
+{
+    private readonly BookstoreDbContext _dbContext;
+
+    public CustomersSeed(BookstoreDbContext dbContext) => _dbContext = dbContext;
+
+    public async Task<Customer> EnsureEqualExists(Customer entity)
+    {
+        if (_dbContext.Customers.FirstOrDefault(c => c.Label == entity.Label) is Customer existing) return existing;
+
+        _dbContext.Customers.Add(entity);
+        await _dbContext.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task SeedAsync()
+    {
+        foreach (Customer customer in DemoCustomers)
+        {
+            await EnsureEqualExists(customer);
+        }
+    }
+
+    private static IEnumerable<Customer> DemoCustomers => new Customer[]
+    {
+        Customer.CreateNew("Joe"),
+        Customer.CreateNew("Jill"),
+        Customer.CreateNew("Jake"),
+        Customer.CreateNew("Jim"),
+        Customer.CreateNew("Jane"),
+        Customer.CreateNew("Jerry"),
+        Customer.CreateNew("Jenny"),
+        Customer.CreateNew("Jesse"),
+    };
+}
