41149f4e72310af3d8a021bc48daad1fe249e721
Zoran Horvat <zh@codinghelmet.com>
2023-05-06 16:28:27 +0200

    Moved authors specification

--- Source/Bookstore/Domain/Specifications/AuthorSpecifications.cs
+++
@@ -1,9 +0,0 @@
-using Bookstore.Domain.Models;
-
-namespace Bookstore.Domain.Specifications;
-
-public static class AuthorSpecifications
-{
-    public static IQueryable<Person> GetPublishedAuthors(this IQueryable<BookAuthor> bookAuthors) =>
-        bookAuthors.Select(bookAuthor => bookAuthor.Person).Distinct();
-}
